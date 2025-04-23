c3ff5b8ff95ef1bcb066375f61c39b8068ad8baf
Ukhang <ukhangmarma459@gmail.com>
2025-04-23 17:32:54 +0600

    home page hero section design

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,11 +1,14 @@
+import Navbar from "./components/navbar";
+
 export default function MarketingLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-        <>
-            {children}
-        </>
-    )
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
 }
--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,9 +1,33 @@
-
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
-    return (
-        <div>
-
+  return (
+    <div className="px-4 max-w-2xl lg:max-w-3xl w-full mx-auto">
+      <div className="z-10 flex flex-col gap-4">
+        <h1 className="text-xl leading-normal md:text-2xl md:leading-[52px] text-custom-foreground md:mt-[80px] lg:mt-[150px] mt-18 text-left justify-start font-medium text-custom-primary">
+          Copy and Use Beautiful Web UI Animations Instantly :)
+        </h1>
+        <p className="text-custom-primary max-w-[480px] text-base w-full">
+          Fluid, natural animations with Framer Motion and Tailwind CSS for
+          exceptional user experiences.
+        </p>
+        <div className="flex gap-2 items-center justify-start">
+          <Image
+            src="/ukhang.png"
+            alt="ukhang"
+            width={30}
+            height={30}
+            className="rounded-full"
+          />
+          <div className="text-sm">
+           <span className="text-muted-foreground">by</span>{" "}
+            <Link href="https://ukhangmarma.vercel.app" target="_blank" className="text-custom-primary">
+              Ukhang Marma
+            </Link>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
