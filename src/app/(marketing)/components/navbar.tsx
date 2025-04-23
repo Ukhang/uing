7240a69208774ba370637a66ffe103ef4c2edab5
Ukhang <ukhangmarma459@gmail.com>
2025-04-23 21:01:26 +0600

    update navbar

--- src/app/(marketing)/components/navbar.tsx
+++ src/app/(marketing)/components/navbar.tsx
@@ -3,11 +3,11 @@
 import { Button } from "@/components/animations/magnetic-button";
 import React from "react";
 import { Github } from "lucide-react";
-import { useRouter } from "next/navigation";
 
 const Navbar = () => {
-
-  const router = useRouter();
+  const handleGitHubRedirect = () => {
+    window.open("https://github.com/Ukhang/uiang", "_blank", "noopener noreferrer");
+  };
 
   return (
     <header className="w-full lg:fixed top-0 left-0">
@@ -16,7 +16,7 @@ const Navbar = () => {
           magnetic
           size={"sm"}
           className="cursor-pointer bg-gradient-to-t from-[#a5e8bb] to-[#c8f8d8] hover:from-[#a9f0c1] hover:to-[#c8f8d8] dark:from-[#97d5ac] dark:to-[#b1dcbf] text-black dark:text-black border dark:hover:from-[#a5e8bb] dark:hover:to-[#c8f8d8] border-[#99e0b3] dark:border-[#87c59e] text-shadow"
-          onClick={() => router.push("https://github.com/Ukhang/uiang")}
+          onClick={handleGitHubRedirect}
         >
           <Github /> Star on GitHub
         </Button>
